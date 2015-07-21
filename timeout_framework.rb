0063b0c608f7b18b4781e9c6f14ece6345049664
Xiaoting <yext4011@gmail.com>
2015-07-20 22:45:08 -0700

    first try on the timeout stuff

---
+++ timeout_framework.rb
@@ -0,0 +1,62 @@
+require 'timeout'
+module TimeoutFramework
+  @filtering      = false
+  TIMEOUT_OPTIONS = {}
+  def timeout(method, options = {:timeout => 1, :default_return => []})
+    TIMEOUT_OPTIONS[method] = options
+  end
+
+  # this is a callback method provided by ruby
+  # there is another method called singleton_method_added, which 
+  # we can also make use of
+  def method_added(method)
+    return if @processing
+    @processing = true
+    alias_method(:"new_#{method}", method)
+    define_method(method) do |*args|
+      return self.send("new_#{method}", *args) if TIMEOUT_OPTIONS[method].nil?
+      options = TIMEOUT_OPTIONS[method]
+      begin
+        Timeout.timeout(options[:timeout]) do
+          # we can also measure the timeout here to make it adapitve
+          time = Time.now
+          ret = self.send(:"new_#{method}", *args)
+          puts Time.now - time 
+        end
+      rescue 
+        $stderr.puts "#{method} takes too long, cut it off"
+        ret = options[:default_return]
+      end
+      ret
+    end
+    @processing = false
+  end
+end
+
+class TimeoutTest
+  extend TimeoutFramework
+  TimeoutTest.timeout :search, :timeout => 1.5,   :default_return => []
+  TimeoutTest.timeout :create, :timeout => 0.5, :default_return => {}
+  def delete
+    sleep(1)
+    puts 'delete succeeded!'
+  end
+  def search
+    puts 'a'
+    sleep(1)
+    puts "search succeeded!"
+    [1]
+  end
+
+  def create
+    sleep(1)
+    {:a => 1}
+  end
+end
+
+if __FILE__ == $0
+  tt = TimeoutTest.new
+  puts tt.delete.inspect
+  puts tt.search.inspect
+  puts tt.create.inspect
+end
